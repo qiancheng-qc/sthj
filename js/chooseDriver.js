737a900caf85ac10d02d3d63f8fe6c1f04cd2fa6
qianCheng <qiancheng0573@163.com>
2021-03-22 17:39:16 +0800

    0322

--- js/chooseDriver.js
+++ js/chooseDriver.js
@@ -1,13 +1,17 @@
 $(function () {
 	var $drivers = $('.drivers')
-	$drivers.on('click', '.driver-info', function () {
-		// console.log($(this).find('.driver-status').children().attr('class'))
-		$(this).find('.driver-input')[0].checked = !$(this).find('.driver-input')[0].checked
-	})
+	var $searchInput = $('.search-box input') // 搜索框
+	var $searchBtn = $('.search-box button') // 搜索按钮
+	var $count = $('.bottom-left span') // 选中司机数量
+	var $confirmBtn = $('.confirm-btn') // 右下角确定按钮
+	var count = 0 // 选中司机数量
+	var axiosInfo // 后台数据
+
+	// 创建司机信息 class="driver-info"
 	function createDriverInfo(id, name, phone, status, statusName) {
 		return `<div class="driver-info">
     <div class="radio">
-      <input class="driver-input" type="radio" name="driver" id="input${id}" />
+      <input class="driver-input" type="checkbox" name="driver" id="input${id}" />
       <label class="mui-icon driver-label" for="input${id}"></label>
     </div>
     <div class="name-phone">
@@ -19,14 +23,7 @@ $(function () {
     </div>
   </div>`
 	}
-	var axiosInfo
-	axiosInfo = [
-		{ id: 1, name: '张三', phone: 13311111111, status: 'notBusy', statusName: '空闲中' },
-		{ id: 2, name: '李四', phone: 13322222222, status: 'notBusy', statusName: '空闲中' },
-		{ id: 3, name: '王五', phone: 13333333333, status: 'notBusy', statusName: '空闲中' },
-		{ id: 4, name: '赵六', phone: 13344444444, status: 'busy', statusName: '已指派' },
-		{ id: 5, name: '周七', phone: 13355555555, status: 'busy', statusName: '已指派' }
-	]
+	// 将创建的司机信息渲染到页面
 	function renderDrivers(arr) {
 		arr.forEach(x => {
 			var infoDiv = createDriverInfo(x.id, x.name, x.phone, x.status, x.statusName)
@@ -35,13 +32,7 @@ $(function () {
 			$drivers.append(infoDiv)
 		})
 	}
-	renderDrivers(axiosInfo)
-	var $searchInput = $('.search-box input')
-	var $searchBtn = $('.search-box button')
-	$searchBtn.on('click', function () {
-		searchDriverBy($searchInput.val())
-		$searchInput.val('')
-	})
+	// 搜索
 	function searchDriverBy(val) {
 		var res = []
 		if (val) {
@@ -51,11 +42,55 @@ $(function () {
 				}
 			})
 			console.log(res)
-			$('.driver-info').remove()
+			$driverInfo.remove()
 			renderDrivers(res)
 		} else {
-			$('.driver-info').remove()
+			$driverInfo.remove()
 			renderDrivers(axiosInfo)
 		}
 	}
+
+	axiosInfo = [
+		{ id: 1, name: '张三', phone: 13311111111, status: 'notBusy', statusName: '空闲中' },
+		{ id: 2, name: '李四', phone: 13322222222, status: 'notBusy', statusName: '空闲中' },
+		{ id: 3, name: '王五', phone: 13333333333, status: 'notBusy', statusName: '空闲中' },
+		{ id: 4, name: '赵六', phone: 13344444444, status: 'busy', statusName: '已指派' },
+		{ id: 5, name: '周七', phone: 13355555555, status: 'busy', statusName: '已指派' }
+	]
+	renderDrivers(axiosInfo)
+
+  // 等司机信息渲染后再执行
+  var $driverInfo = $('.driver-info')
+  var $driverInputs = $('.driver-input')
+
+	$searchBtn.on('click', function () {
+		searchDriverBy($searchInput.val())
+		$searchInput.val('')
+	})
+
+	// 选中司机
+	$drivers.on('click', '.driver-info', function () {
+		// 司机空闲中点击可添加
+		var statusClassName = $(this).find('.driver-status').children().attr('class')
+		if (statusClassName === 'notBusy') {
+			$(this).find('.driver-input')[0].checked = !$(this).find('.driver-input')[0].checked
+		}
+		count = 0
+		var i = 0
+		// 遍历所有多选框
+		for (let key in $driverInputs) {
+			if (i < $driverInputs.length) {
+				i++
+				if ($driverInputs[key].checked) {
+					count++
+					// 渲染选中司机数
+					$count[0].innerText = count
+				}
+			}
+		}
+	})
+
+	$confirmBtn.on('click', function () {
+		console.log('确定')
+	})
 })
